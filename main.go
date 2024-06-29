f4fefc99cb0c8621277456eba473964b173cbc4c
Denis Zakharov <dizaharov@gmail.com>
2024-06-29 09:55:41 +0200

    simple http handler

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func main() {
+	http.HandleFunc("/double", doubleHandler)
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+}
+
+func doubleHandler(w http.ResponseWriter, r *http.Request) {
+	text := r.FormValue("v")
+	if text == "" {
+		http.Error(w, "missing value", http.StatusBadRequest)
+		return
+	}
+
+	v, err := strconv.Atoi(text)
+	if err != nil {
+		http.Error(w, "not a number: "+text, http.StatusBadRequest)
+	}
+
+	fmt.Fprintln(w, v*2)
+}
